e0f1049768e4e2a2d09422111cb14644dd065212
zkerriga <daniil.63@yandex.ru>
2021-09-26 18:30:24 +0300

    Add define TOP

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -2,6 +2,6 @@
  * Comment
  */
 #include <stdio.h>
-
-#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, #code"\nFT("#code")"); fclose(fp); return 0; }
-FT(#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, #code"\nFT("#code")"); fclose(fp); return 0; })
+#define TOP "/*\n * Comment\n */\n#include <stdio.h>\n"
+#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, TOP"#define TOP test\n"#code"\nFT("#code")"); fclose(fp); return 0; }
+FT(#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, TOP"#define TOP test\n"#code"\nFT("#code")"); fclose(fp); return 0; })
