d95f6ca95dcb56256bb1c7b7832dc795be5521cb
zkerriga <daniil.63@yandex.ru>
2021-09-26 18:50:19 +0300

    Writing file works

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -3,5 +3,5 @@
  */
 #include <stdio.h>
 #define TOP "/*\n * Comment\n */\n#include <stdio.h>\n"
-#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s#define TOP %s\n%s\nFT(%s)", TOP, "test", #code, #code); fclose(fp); return 0; }
-FT(#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s#define TOP %s\n%s\nFT(%s)", TOP, "test", #code, #code); fclose(fp); return 0; })
+#define FT(code) const char * f() { return "\"/*\\n * Comment\\n */\\n#include <stdio.h>\\n\""; } int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s#define TOP %s\n%s\nFT(%s)\n", TOP, f(), #code, #code); fclose(fp); return 0; }
+FT(#define FT(code) const char * f() { return "\"/*\\n * Comment\\n */\\n#include <stdio.h>\\n\""; } int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s#define TOP %s\n%s\nFT(%s)\n", TOP, f(), #code, #code); fclose(fp); return 0; })
