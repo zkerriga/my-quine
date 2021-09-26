ea3ad1d79a0ebd2ed9f674445025e21273909934
zkerriga <daniil.63@yandex.ru>
2021-09-26 18:07:32 +0300

    Write part of program

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,3 +1,7 @@
+/*
+ * Comment
+ */
 #include <stdio.h>
-#define FT(x) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s", "source_code"); fclose(fp); return 0; }
-FT(xxxxx)
+
+#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, #code"\nFT("#code")"); fclose(fp); return 0; }
+FT(#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, #code"\nFT("#code")"); fclose(fp); return 0; })
