3282c3838464f6a0017d794452dbebbc3ff773e4
zkerriga <daniil.63@yandex.ru>
2021-09-26 18:42:30 +0300

    Add TOP without body

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -3,5 +3,5 @@
  */
 #include <stdio.h>
 #define TOP "/*\n * Comment\n */\n#include <stdio.h>\n"
-#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, TOP"#define TOP test\n"#code"\nFT("#code")"); fclose(fp); return 0; }
-FT(#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, TOP"#define TOP test\n"#code"\nFT("#code")"); fclose(fp); return 0; })
+#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s#define TOP %s\n%s\nFT(%s)", TOP, "test", #code, #code); fclose(fp); return 0; }
+FT(#define FT(code) int main() { FILE *fp = fopen("Grace_kid.c", "w"); fprintf(fp, "%s#define TOP %s\n%s\nFT(%s)", TOP, "test", #code, #code); fclose(fp); return 0; })
